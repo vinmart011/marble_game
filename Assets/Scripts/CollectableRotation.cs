0f33b677adef0b370db34c43215ce96a0012b551
Vincent <vmart044@ucr.edu>
2026-01-09 13:52:33 -0800

    Adding pickup scoring

--- Assets/Scripts/CollectableRotation.cs
+++ Assets/Scripts/CollectableRotation.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CollectableRotation : MonoBehaviour
 {
 
-    public Vector3 rotation = new Vector3(15,30,45);
+    public Vector3 rotation = new Vector3(30,40,45);
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,16 @@
 
 using UnityEngine; // this gives us access to monobehaviour, gameobject, transform, ridigbody, vector3, etc.
 using UnityEngine.InputSystem; 
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
+    public TMP_Text scoreDisplay;
     public float speed = 10.0f;
 
     private Rigidbody rb;
+    private int PickupCnt = 0;
 
     private float movementX;
     private float movementY;
@@ -38,6 +41,13 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            PickupCnt++;
+            SetScoreText();
         }
+    }
+
+    void SetScoreText()
+    {
+        scoreDisplay.text = "Pickups Acquired: " + PickupCnt.ToString();
     }
 }
