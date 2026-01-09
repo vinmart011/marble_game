436a14ab2a3521e2ff5b7b4b50877496d1b306d8
Vincent <vmart044@ucr.edu>
2026-01-09 15:42:08 -0800

    Fiddling with player input camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
 
     public GameObject player; //  reference to player object
     private Vector3 offset; //  camera offset for player
+    private InputAction lookAction;
+    private float yaw;
 
     void Start()
     {
         offset = transform.position - player.transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+        lookAction = InputSystem.actions.FindAction("Look");
     }
     
     void Update()
     {
-        transform.position = offset + player.transform.position;
+        
+        Vector2 look = lookAction.ReadValue<Vector2>();
+        yaw += look.x * 100f * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0f, yaw, 0f);
+        transform.position = player.transform.position + rotation * offset;
+        transform.LookAt(player.transform.position);
+
     }
 
 }
